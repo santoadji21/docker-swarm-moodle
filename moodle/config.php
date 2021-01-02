d0f08310b28fbb4c6e3e5afab5c2c8e533646e44
santoadji21 <ahmad.aji.santoso21@gmail.com>
2021-01-02 15:19:50 +0700

    Initial Commmit

---
+++ moodle/config.php
@@ -0,0 +1,31 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysql';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'moodle_db';
+$CFG->dbname    = 'moodle';
+$CFG->dbuser    = 'moodle';
+$CFG->dbpass    = 'r00t';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 5432,
+  'dbsocket' => '',
+);
+
+$CFG->wwwroot   = 'http://192.168.56.10';
+$CFG->dataroot  = '/var/www/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+$CFG->sslproxy=0;
+$CFG->loginhttps=1;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
